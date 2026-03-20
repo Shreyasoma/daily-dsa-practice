f2845c3b94f48c7fe3e9fdf388d31e46b90ae1be
Shreya <shreyaforany@gmail.com>
2026-03-20 21:07:00 +0530

    Addition of two numbers Function

---
+++ SumFunction/Main.java
@@ -0,0 +1,19 @@
+// import java.util.Scanner;
+
+public class Main {
+    public static int sum(int a, int b) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter num1:");
+        // int num1 = sc.nextInt();
+        // System.out.print("Enter num2:");
+        // int num2 = sc.nextInt();
+        // int sum = num1 + num2;
+        // System.out.println("Sum:" + sum);
+        return a + b;
+    }
+
+    public static void main(String[] main) {
+        System.out.println(sum(6, 4));
+        System.out.println(sum(10, 30));
+    }
+}
